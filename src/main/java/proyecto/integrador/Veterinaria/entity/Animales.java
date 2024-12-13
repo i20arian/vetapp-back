170dde0f7700eff7d28cb5eda4f9f6b0b4a103aa
Arian Vega <i201721882@cibertec.edu.pe>
2024-12-13 01:40:14 -0500

    Se dio preferencia a las anotaciones lombok y se elimino el uso de la anotacion @Data ya que no es necesario en Animales.java

--- src/main/java/proyecto/integrador/Veterinaria/entity/Animales.java
+++ src/main/java/proyecto/integrador/Veterinaria/entity/Animales.java
@@ -1,12 +1,17 @@
 package proyecto.integrador.Veterinaria.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@AllArgsConstructor
-@Data
 @Table(name = "animales")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Animales {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,83 +24,4 @@ public class Animales {
   private String informacionAnimal;
   private String generoAnim;
   private String tipoAnimal;
-
-  public Animales(Long codigoAnimal, String dueno, String nombreAnimal, int edad, String informacionAnimal,
-      double peso, String generoAnim, String tipoAnimal) {
-    this.codigoAnimal = codigoAnimal;
-    this.dueno = dueno;
-    this.nombreAnimal = nombreAnimal;
-    this.edad = edad;
-    this.informacionAnimal = informacionAnimal;
-    this.peso = peso;
-    this.generoAnim = generoAnim;
-    this.tipoAnimal = tipoAnimal;
-  }
-
-  public Animales() {
-  }
-
-  public String getDueno() {
-    return dueno;
-  }
-
-  public void setDueno(String dueno) {
-    this.dueno = dueno;
-  }
-
-  public String getNombreAnimal() {
-    return nombreAnimal;
-  }
-
-  public void setNombreAnimal(String nombreAnimal) {
-    this.nombreAnimal = nombreAnimal;
-  }
-
-  public Long getCodigoAnimal() {
-    return codigoAnimal;
-  }
-
-  public void setCodigoAnimal(Long codigoAnimal) {
-    this.codigoAnimal = codigoAnimal;
-  }
-
-  public int getEdad() {
-    return edad;
-  }
-
-  public void setEdad(int edad) {
-    this.edad = edad;
-  }
-
-  public double getPeso() {
-    return peso;
-  }
-
-  public void setPeso(double peso) {
-    this.peso = peso;
-  }
-
-  public String getInformacionAnimal() {
-    return informacionAnimal;
-  }
-
-  public void setInformacionAnimal(String informacionAnimal) {
-    this.informacionAnimal = informacionAnimal;
-  }
-
-  public String getGeneroAnim() {
-    return generoAnim;
-  }
-
-  public void setGeneroAnim(String generoAnim) {
-    this.generoAnim = generoAnim;
-  }
-
-  public String getTipoAnimal() {
-    return tipoAnimal;
-  }
-
-  public void setTipoAnimal(String tipoAnimal) {
-    this.tipoAnimal = tipoAnimal;
-  }
 }
