2a16f3b8452754fed15b8e0a4f33e412aed441d9
Arian Vega <i201721882@cibertec.edu.pe>
2024-12-13 01:50:19 -0500

    Se dio preferencia a las anotaciones lombok y se elimino el uso de la anotacion @Data ya que no es necesario en Doctor.java

--- src/main/java/proyecto/integrador/Veterinaria/entity/Doctor.java
+++ src/main/java/proyecto/integrador/Veterinaria/entity/Doctor.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Doctor {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,65 +28,4 @@ public class Doctor {
   private String especialidad;
 
   private String experiencia;
-
-  public Doctor() {
-  }
-
-  public Doctor(int codigoDoctor, String nombreDoctor, int dniDoctor, String apellidoDoctor, String especialidad,
-      String experiencia) {
-    this.codigoDoctor = codigoDoctor;
-    this.nombreDoctor = nombreDoctor;
-    this.dniDoctor = dniDoctor;
-    this.apellidoDoctor = apellidoDoctor;
-    this.especialidad = especialidad;
-    this.experiencia = experiencia;
-  }
-
-  public int getCodigoDoctor() {
-    return codigoDoctor;
-  }
-
-  public void setCodigoDoctor(int codigoDoctor) {
-    this.codigoDoctor = codigoDoctor;
-  }
-
-  public String getNombreDoctor() {
-    return nombreDoctor;
-  }
-
-  public void setNombreDoctor(String nombreDoctor) {
-    this.nombreDoctor = nombreDoctor;
-  }
-
-  public String getApellidoDoctor() {
-    return apellidoDoctor;
-  }
-
-  public void setApellidoDoctor(String apellidoDoctor) {
-    this.apellidoDoctor = apellidoDoctor;
-  }
-
-  public int getDniDoctor() {
-    return dniDoctor;
-  }
-
-  public void setDniDoctor(int dniDoctor) {
-    this.dniDoctor = dniDoctor;
-  }
-
-  public String getEspecialidad() {
-    return especialidad;
-  }
-
-  public void setEspecialidad(String especialidad) {
-    this.especialidad = especialidad;
-  }
-
-  public String getExperiencia() {
-    return experiencia;
-  }
-
-  public void setExperiencia(String experiencia) {
-    this.experiencia = experiencia;
-  }
 }
