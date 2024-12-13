3bcef14d7c4a86cda37556f10252daad1e541a3e
Arian Vega <i201721882@cibertec.edu.pe>
2024-12-13 01:45:09 -0500

    Se dio preferencia a las anotaciones lombok y se elimino el uso de la anotacion @Data ya que no es necesario en Cliente.java

--- src/main/java/proyecto/integrador/Veterinaria/entity/Cliente.java
+++ src/main/java/proyecto/integrador/Veterinaria/entity/Cliente.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Cliente {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,65 +28,4 @@ public class Cliente {
   private String informacionCliente;
 
   private String generoClie;
-
-  public Cliente() {
-  }
-
-  public Cliente(int codigoCliente, String nombreCliente, String apellidosCliente, int dniCliente,
-      String informacionCliente, String generoClie) {
-    this.codigoCliente = codigoCliente;
-    this.nombreCliente = nombreCliente;
-    this.apellidosCliente = apellidosCliente;
-    this.dniCliente = dniCliente;
-    this.informacionCliente = informacionCliente;
-    this.generoClie = generoClie;
-  }
-
-  public int getCodigoCliente() {
-    return codigoCliente;
-  }
-
-  public void setCodigoCliente(int codigoCliente) {
-    this.codigoCliente = codigoCliente;
-  }
-
-  public String getNombreCliente() {
-    return nombreCliente;
-  }
-
-  public void setNombreCliente(String nombreCliente) {
-    this.nombreCliente = nombreCliente;
-  }
-
-  public int getDniCliente() {
-    return dniCliente;
-  }
-
-  public void setDniCliente(int dniCliente) {
-    this.dniCliente = dniCliente;
-  }
-
-  public String getApellidosCliente() {
-    return apellidosCliente;
-  }
-
-  public void setApellidosCliente(String apellidosCliente) {
-    this.apellidosCliente = apellidosCliente;
-  }
-
-  public String getInformacionCliente() {
-    return informacionCliente;
-  }
-
-  public void setInformacionCliente(String informacionCliente) {
-    this.informacionCliente = informacionCliente;
-  }
-
-  public String getGeneroClie() {
-    return generoClie;
-  }
-
-  public void setGeneroClie(String generoClie) {
-    this.generoClie = generoClie;
-  }
 }
