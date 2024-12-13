c14567e17487ca08fc2b172d20b8b6bda54d1254
Arian Vega <i201721882@cibertec.edu.pe>
2024-12-13 03:31:26 -0500

    Se cambio el tipo de dato int a Long de la propiedad id de Cliente.java

--- src/main/java/proyecto/integrador/Veterinaria/entity/Cliente.java
+++ src/main/java/proyecto/integrador/Veterinaria/entity/Cliente.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 public class Cliente {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
+  private Long id;
   private String nombres;
   private String apellidos;
   private int dni;
